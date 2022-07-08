110c277efe07c3fad3d066f7ce6980b07e009ef0
Piyush Goswami <71273344+piyushcodes6@users.noreply.github.com>
2022-07-08 13:41:04 +0530

    Update index.js
    Updated index.js with new entries

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const port=3333;
 const mongoose=require('mongoose');
 const registerUsersRoutes=require('./routes/registerUsersRoutes')
-const loginRoutes=require('./routes/loginRoutes')
+const createVideoRoutes=require('./routes/createVideoRoutes')
 app.use(express.json());
 
 app.use('/registerUsers',registerUsersRoutes);
-app.use('/login',loginRoutes)
+app.use('/createVideo',createVideoRoutes);
 mongoose.connect("mongodb://127.0.0.1:27017/?readPreference=primary&directConnection=true&ssl=false")
 .then(() =>{
     console.log(`Db connected`);    
